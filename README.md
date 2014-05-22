f15eada3a7406a1e69a0b3c65222604807f73e4b
Mark Williams <markwilliams@mwilliams-imac.f4tech.com>
2014-05-22 12:53:26 -0600

    Updated create_scm.rb to be interactive.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 changeset-utils
 ===============
 
-Simple Changeset create/delete utility for Rally
+clean_changeset.rb
+==========================
+
+Utility for listing Changesets associated with a specified Rally Artifact, selected by FormattedID.
+User may delete selected Changesets from the Artifact, if Changesets are present.
+Note: Deletion is permanent! Changesets deleted via this method cannot be recovered.
+ 
+create_changeset.rb
+================================
+
+This script allows the user to create a Changeset associated to a Rally Artifact, selected by FormattedID.
+The user is prompted to enter a Revision Number and Commit Message. Other parameters, such as SCM Repo and
+the SCM URL, are specified in the my_vars.rb file
+
+create_scm.rb
+================================
+
+This script allows the user to create an SCM Repo in a Workspace as specified in the my_vars.rb file.
+The user is prompted to enter the SCM name, description, URL, and type.
+
+Note! Build and Changesets must be enabled on the target workspace in order for the create to succeed. This can
+be done by a Rally Subscription or Workspace Administrator.
+
+Requirements:
+
+ - Ruby 1.9.3 or higher
+ - rally_api 1.0 or higher
--- create_scm.rb
+++ create_scm.rb
@@ -3,17 +3,17 @@ require 'date'
 require 'time'
 
 # Load (and maybe override with) my personal/private variables from a file...
-my_vars= File.dirname(__FILE__) + "/my_vars2.rb"
+my_vars= File.dirname(__FILE__) + "/my_vars.rb"
 if FileTest.exist?( my_vars ); print "Loading <#{my_vars}>...\n"; require my_vars end
 
 #==================== Making a connection to Rally ====================#
 puts "Connecting to Rally: #{$my_base_url} as #{$my_username}..."
 
-$headers                                        = RallyAPI::CustomHttpHeader.new()
-$headers.name                           = "Create SCMRepo"
-$headers.vendor                         = "KateT"
-$headers.version                         = "Alpha"
-$my_headers                                = $headers
+$headers                = RallyAPI::CustomHttpHeader.new()
+$headers.name           = "Create SCMRepo"
+$headers.vendor         = "Rally Labs"
+$headers.version        = "0.50"
+$my_headers             = $headers
 
 config                  = {:base_url => $my_base_url}
 config[:username]       = $my_username
@@ -29,11 +29,46 @@ config[:headers]        = $my_headers #from RallyAPI::CustomHttpHeader.new()
 puts "Now connected to Rally."
 
 #==================== Creating an SCM Repository ====================
-    fields = {}
-      fields["Name"] = "SCMRepo"
-      fields["Description"] = "My SCMRepository"
-      fields["Uri"] = "https://somerepo.company.com:8080"
-      fields["SCMType"] = "git"
+fields = {}
+fields["Name"] = "SCMRepo"
+fields["Description"] = "My SCMRepository"
+fields["Uri"] = "https://somerepo.company.com:8080"
+fields["SCMType"] = "git"
 
-scm_repo_create= @rally.create(:SCMRepository, fields)
-puts scm_repo_create["Name"  ]
+# Ask user for Artifact Formatted ID
+puts "Please enter a name for the SCM Repository to create:"
+puts
+scm_repo_name = [(print 'Enter Name: '), gets.rstrip][1]
+
+puts "Please enter a brief Description for the SCM Repository:"
+puts
+scm_repo_desc = [(print 'Enter Description: '), gets.rstrip][1]
+
+puts "Please enter a URL for the SCM Repository (i.e. https://websvn.company.com:8080):"
+puts
+scm_repo_uri = [(print 'Enter URL: '), gets.rstrip][1]
+
+puts "Please enter a Type for the SCM Repository (i.e. Git, SVN, etc.):"
+puts
+scm_repo_type = [(print 'Enter Type: '), gets.rstrip][1]
+
+if scm_repo_name.nil? || scm_repo_desc.nil? || scm_repo_uri.nil? || scm_repo_type.nil? then
+  puts "Missing required entry! Will exit."
+end
+
+fields["Name"]        = scm_repo_name
+fields["Description"] = scm_repo_desc
+fields["Uri"]         = scm_repo_uri
+fields["SCMType"]     = scm_repo_type
+
+begin
+  scm_repo_create = @rally.create(:SCMRepository, fields)
+  puts "Successfully Created #{scm_repo_name}."
+rescue => ex
+  puts "Error occurred trying to create: #{scm_repo_name}."
+  puts ex
+  puts ex.msg
+  puts ex.backtrace
+end
+
+puts "Complete!"
